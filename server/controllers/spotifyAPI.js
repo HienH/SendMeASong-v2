e51fbd08f9a4a949981b87436146807534f4464d
hien hang <hienhang@hotmail.co.uk>
2020-03-20 13:52:40 +0000

    add promise

--- server/controllers/spotifyAPI.js
+++ server/controllers/spotifyAPI.js
@@ -1,36 +1,44 @@
-var request = require('request');
+var request = require('request-promise');
+// var request = require('request');
 
-module.exports.loginSpotify = (req, res) => {
-    // Get code
-    const url = req.headers.referer
-    let urlSplit = url.indexOf('=');
-    let code = url.slice(urlSplit + 1);
+module.exports.loginSpotify = ((req, res) => {
 
-    var headers = {
-        'Authorization': 'Basic MTY3ZTBiZGM1MWEyNDFjOWExYzc4MWIwZjhjM2RmN2Y6YWI4MWY4MTA3NDk3NDkwOThlNTExYTU0ZjA2OGIxNTU=',
-        'Accept': 'application/json',
-        'Content-Type': 'application/x-www-form-urlencoded',
-    };
+    const getToken = new Promise((resolve, reject) => {
+        // Get code
+        const url = req.headers.referer
+        let urlSplit = url.indexOf('=');
+        let code = url.slice(urlSplit + 1);
 
-    var dataString = 'grant_type=authorization_code&code=' + code + '&redirect_uri=http%3A%2F%2Flocalhost%3A4200%2Fhome';
+        var headers = {
+            'Authorization': 'Basic MTY3ZTBiZGM1MWEyNDFjOWExYzc4MWIwZjhjM2RmN2Y6YWI4MWY4MTA3NDk3NDkwOThlNTExYTU0ZjA2OGIxNTU=',
+            'Accept': 'application/json',
+            'Content-Type': 'application/x-www-form-urlencoded',
+        };
 
-    var options = {
-        url: 'https://accounts.spotify.com/api/token',
-        method: 'POST',
-        headers: headers,
-        body: dataString
-    };
+        var dataString = 'grant_type=authorization_code&code=' + code + '&redirect_uri=http%3A%2F%2Flocalhost%3A4200%2Fhome';
 
-    request(options, (error, response, body) => {
-        if (response.statusCode == 200) {
-            res.send(body);
-        } else {
-            res.send(error);
-        }
-    });
-};
+        var options = {
+            url: 'https://accounts.spotify.com/api/token',
+            method: 'POST',
+            headers: headers,
+            body: dataString
+        };
 
 
+        request(options, (error, response, body) => {
+            if (response.statusCode == 200) {
+                resolve(body.access_token);
+            }
+            reject(error)
+        });
+    })
+
+    getToken.then((res) => {
+        console.log(res);
+        let token = res
+        console.log(token);
+    })
+});
 
 //GET ID
 // https://api.spotify.com/v1/me
@@ -46,7 +54,5 @@ module.exports.loginSpotify = (req, res) => {
 
 // ADD TRACK 
 // https://api.spotify.com/v1/playlists/{plauylistID}/tracks?uris=spotify%3Atrack%3A{trackURIS}
-
-
 
 
